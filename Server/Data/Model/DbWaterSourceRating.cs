236f638d682c62ff79bd96c55cdc66c70a0841bf
Mohayemin <mohayeminul.islam@jantrik.com>
2015-04-08 18:18:40 +0600

    WaterSource db models added

---
+++ Server/Data/Model/DbWaterSourceRating.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Data.Model.Base;
+using Data.Model.Constants;
+
+namespace Data.Model
+{
+    [Table("WaterSourceRating")]
+    public class DbWaterSourceRating : DbModel
+    {
+        public int WaterSourceId { get; set; }
+        public Potability Potability { get; set; }
+        public int? UserId { get; set; }
+    }
+}
