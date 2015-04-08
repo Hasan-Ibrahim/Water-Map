dff4559dba8574a2cecdf889c39a428a86056440
Mohayemin <mohayeminul.islam@jantrik.com>
2015-04-08 18:18:40 +0600

    WaterSource db models added

--- Server/Data/Model/DbWaterSource.cs
+++ Server/Data/Model/DbWaterSource.cs
@@ -9,5 +9,8 @@ namespace Data.Model
     {
         public string SourceType { get; set; }
         public DbGeometry Geometry { get; set; }
+        public int PotableRatingCount { get; set; }
+        public int ProcessableRatingCount { get; set; }
+        public int UnpotableRatingCount { get; set; }
     }
 }
