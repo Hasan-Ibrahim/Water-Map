e35818e06abcdc7b951897953900145ab8a3c243
Mohayemin <mohayeminul.islam@jantrik.com>
2015-04-09 15:38:52 +0600

    Dependency config refactored

--- Server/Api/App_Start/UnityConfig.cs
+++ Server/Api/App_Start/UnityConfig.cs
@@ -22,38 +22,59 @@ namespace Api
         {
             var container = new UnityContainer();
 
+            ConfigureOAuth(container);
+            ConfigureRedis(container);
+            ConfigureTokenStorage(container, false, false);
+            ConfigureRepository(container, false);
 
-            //container.RegisterType<ITokenStorage, PgsqlTokenStorage>();
-            //container.RegisterType(typeof (IRepository<>), typeof (PgsqlRepository<>));
+            container.RegisterType<HttpContext>(new InjectionFactory(unityContainer => HttpContext.Current));            
+            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+        }
+
+        private static void ConfigureOAuth(UnityContainer container)
+        {
             var _googleClientId = ConfigurationManager.AppSettings["oauthclientid-google"];
             var _googleClientSecret = ConfigurationManager.AppSettings["oauthclientsecret-google"];
-            var _keywordIndexDirectory = HttpContext.Current.Server.MapPath(ConfigurationManager.AppSettings["KeywordIndexDirectory"]);
+            container.RegisterType<GoogleOAuthClient>(new InjectionConstructor(_googleClientId, _googleClientSecret));
+            //container.RegisterType<FacebookOAuthClient>(new InjectionConstructor(_facebookClientId, _facebookClientSecret));
+        }
 
+        private static void ConfigureRepository(UnityContainer container, bool useInMemory)
+        {
+            if (useInMemory)
+            {
+                container.RegisterType(typeof(IRepository<>), typeof(InMemoryRepository<>));
+            }
+            else
+            {
+                container.RegisterType(typeof(IRepository<>), typeof(DbContextRepository<>));
+                container.RegisterType<DbContext, AppDbContext>(new InjectionConstructor("app"));
+            }
+        }
+
+        private static void ConfigureTokenStorage(UnityContainer container, bool useInMemory, bool useRadis)
+        {
+            if (useInMemory)
+            {
+                container.RegisterType<ITokenStorage, InMemoryTokenStorage>();
+            }
+            else if (useRadis)
+            {
+                container.RegisterType<ITokenStorage, RedisTokenStorage>();
+            }
+            else
+            {
+                container.RegisterType<ITokenStorage, DbContextTokenStorage>();                
+            }
+        }
+
+        private static void ConfigureRedis(UnityContainer container)
+        {
             var redisServerHost = ConfigurationManager.AppSettings["redisServerHost"];
             var redisServerPort = Convert.ToInt32(ConfigurationManager.AppSettings["redisServerPort"]);
             var redisServerPassword = ConfigurationManager.AppSettings["redisServerPassword"];
-
-            container.RegisterType<HttpContext>(new InjectionFactory(unityContainer => HttpContext.Current));
-            //container.RegisterType<ITokenStorage, InMemoryTokenStorage>();
-            //container.RegisterType(typeof(IRepository<>), typeof(InMemoryRepository<>));
-
-            // container.RegisterType<ITokenStorage, PgsqlTokenStorage>();
-            container.RegisterType<IRedisClient, RedisClient>(new InjectionConstructor(redisServerHost, redisServerPort, redisServerPassword, 0L));
-            //container.RegisterType<ITokenStorage, RedisTokenStorage>();
-            container.RegisterType<ITokenStorage, DbContextTokenStorage>();
-            //container.RegisterType<ITokenStorage, InMemoryTokenStorage>();
-
-            container.RegisterType(typeof(IRepository<>), typeof(DbContextRepository<>));
-            container.RegisterType<DbContext, AppDbContext>(new InjectionConstructor("app"));
-            //container.RegisterType(typeof(IRepository<>), typeof(InMemoryRepository<>));
-
-            container.RegisterType<GoogleOAuthClient>(new InjectionConstructor(_googleClientId, _googleClientSecret));
-            container.RegisterType<KeywordRepository>(new InjectionConstructor(_keywordIndexDirectory));
-            //container.RegisterType<FacebookOAuthClient>(new InjectionConstructor(_facebookClientId, _facebookClientSecret));
-
-            container.RegisterType(typeof(DbContextRepository<DbUser>), new InjectionConstructor(new ResolvedParameter<AppDbContext>()));
-            
-            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+            container.RegisterType<IRedisClient, RedisClient>(new InjectionConstructor(redisServerHost, redisServerPort,
+                redisServerPassword, 0L));
         }
     }
 }
