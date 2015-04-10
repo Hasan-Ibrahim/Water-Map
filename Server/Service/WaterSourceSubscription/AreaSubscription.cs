99d61c77d8c7c4fb25f156f396490146c5513241
Mohayemin <mohayeminul.islam@jantrik.com>
2015-04-11 00:27:22 +0600

    Model updated

--- Server/Service/WaterSourceSubscription/AreaSubscription.cs
+++ Server/Service/WaterSourceSubscription/AreaSubscription.cs
@@ -6,6 +6,6 @@ namespace Service.WaterSourceSubscription
     public class AreaSubscription
     {
         public string Geometry { get; set; }
-        public IList<WaterSubscriptionType> SubscriptionTypes { get; set; }
+        public Dictionary<WaterSubscriptionType, bool> Subscriptions { get; set; }
     }
 }
