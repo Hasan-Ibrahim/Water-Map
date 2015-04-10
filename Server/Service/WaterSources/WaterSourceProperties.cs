ab2372dce0bc1c10de1c179ecd5aeff44ab88e5c
Mohayemin <mohayeminul.islam@jantrik.com>
2015-04-10 20:48:20 +0600

    Source rating is sent by percentage

--- Server/Service/WaterSources/WaterSourceProperties.cs
+++ Server/Service/WaterSources/WaterSourceProperties.cs
@@ -6,10 +6,10 @@ namespace Service.WaterSources
     public class WaterSourceProperties
     {
         public string SourceType { get; set; }
-        public int PotableRatingCount { get; set; }
-        public int ProcessableRatingCount { get; set; }
-        public int UnpotableRatingCount { get; set; }
-        public int UnknownRatingCount { get; set; }
+        public double PotableRatingPercent { get; set; }
+        public double ProcessableRatingPercent { get; set; }
+        public double UnpotableRatingPercent { get; set; }
+        public double UnknownRatingPercent { get; set; }
         public string[] ImageUrls { get; set; }
         public Accessibility Accessibility { get; set; }
 
@@ -18,13 +18,20 @@ namespace Service.WaterSources
             var imageUrls = string.IsNullOrWhiteSpace(dbWaterSource.ImageUrls)
                 ? new string[] {}
                 : dbWaterSource.ImageUrls.Split('^');
+
+            double totalRatingPercent = dbWaterSource.PotableRatingCount + dbWaterSource.ProcessableRatingCount +
+                                   dbWaterSource.UnpotableRatingCount + dbWaterSource.UnknownRatingCount;
+            totalRatingPercent = totalRatingPercent / 100;
+
+            totalRatingPercent = totalRatingPercent == 0 ? .000001 : totalRatingPercent;
+
             return new WaterSourceProperties
             {
                 SourceType = dbWaterSource.SourceType,
-                PotableRatingCount = dbWaterSource.PotableRatingCount,
-                ProcessableRatingCount = dbWaterSource.ProcessableRatingCount,
-                UnpotableRatingCount = dbWaterSource.UnpotableRatingCount,
-                UnknownRatingCount = dbWaterSource.UnpotableRatingCount,
+                PotableRatingPercent = dbWaterSource.PotableRatingCount / totalRatingPercent,
+                ProcessableRatingPercent = dbWaterSource.ProcessableRatingCount / totalRatingPercent,
+                UnpotableRatingPercent = dbWaterSource.UnpotableRatingCount / totalRatingPercent,
+                UnknownRatingPercent = dbWaterSource.UnpotableRatingCount / totalRatingPercent,
                 ImageUrls = imageUrls,
                 Accessibility = dbWaterSource.Accessibility
             };
