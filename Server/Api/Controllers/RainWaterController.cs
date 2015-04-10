c6880da5412d92d53d246bad2b7fc1dfea2a75b5
Mohayemin <mohayeminul.islam@jantrik.com>
2015-04-11 02:45:04 +0600

    API Post RainWater/AddRainHarvestTank exposed.

---
+++ Server/Api/Controllers/RainWaterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using Service.RainWater;
+
+namespace Api.Controllers
+{
+    public class RainWaterController : ApiController
+    {
+        private readonly RainWaterService _rainWaterService;
+
+        public RainWaterController(RainWaterService rainWaterService)
+        {
+            _rainWaterService = rainWaterService;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _rainWaterService.Dispose();
+            base.Dispose(disposing);
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddRainHarvestTank(RainHarvestTankEntry rainHarvestTankEntry)
+        {
+            return Ok(_rainWaterService.AddRainHarvestTank(rainHarvestTankEntry));
+        }
+    }
+}
---
+++ Server/Service/RainWater/RainWaterService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Data.Model;
+using Data.Repositories.Abstraction;
+
+namespace Service.RainWater
+{
+    public class RainWaterService : IDisposable
+    {
+        private readonly IRepository<DbRainHarvestTank> _rainHarvestTankRepository;
+
+        public RainWaterService(IRepository<DbRainHarvestTank> rainHarvestTankRepository)
+        {
+            _rainHarvestTankRepository = rainHarvestTankRepository;
+        }
+
+        public void Dispose()
+        {
+            _rainHarvestTankRepository.Dispose();
+        }
+
+        public DbRainHarvestTank AddRainHarvestTank(RainHarvestTankEntry rainHarvestTankEntry)
+        {
+            var dbRainHarvestTank = rainHarvestTankEntry.ToDbRainHarvestTank();
+            _rainHarvestTankRepository.Create(dbRainHarvestTank);
+            _rainHarvestTankRepository.SaveChanges();
+            return dbRainHarvestTank;
+        }
+    }
+}
