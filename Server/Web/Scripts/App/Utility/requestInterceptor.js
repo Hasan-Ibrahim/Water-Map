696c5af173833c86659f1a099d189a333e9a5948
hasan ibrahim <shuvoiit0208@gmail.com>
2015-04-11 15:59:18 +0600

    issue fixed

---
+++ Server/Web/Scripts/App/Utility/requestInterceptor.js
@@ -0,0 +1,20 @@
+﻿utilModule.config([
+    '$httpProvider',
+    function ($httpProvider) {
+        $httpProvider.interceptors.push(function () {
+            return {
+                'request': function (config) {
+
+                    var headers = (function getSecurityHeaders() {
+                        return { 'authentication': localStorage.getItem('authentication') || sessionStorage.getItem('authentication') };
+                    })();
+
+                    config.headers = config.headers || {};
+                    for (var i in headers) {
+                        config.headers[i] = headers[i];
+                    }
+                    return config;
+                }
+            }
+        });
+    }]);
---
+++ Server/Web/Scripts/Test/accountRepository.specs.js
@@ -0,0 +1,79 @@
+﻿///<reference path="~/Scripts/Test/scriptReferences.js"/>
+
+describe("accountRepository unit testing.", function () {
+    beforeEach(function () {
+        module('appModule');
+    });
+
+    it("should have a register method.", inject(function (accountRepository) {
+        expect(accountRepository.register).toBeDefined();
+    }));
+
+    it('should post registration success',
+       inject(function (accountRepository, urlResolver, $httpBackend) {
+
+           $httpBackend.expect('POST', urlResolver.resolveAccount('Register'))
+               .respond(200, { success: true });
+
+           accountRepository.register({ loginId: 'test@test.com', password: 'password' })
+               .success(function (data) {
+                   expect(data.success).toBeTruthy();
+               });
+
+           $httpBackend.flush();
+       }));
+
+    it("should have a login method.", inject(function (accountRepository) {
+        expect(accountRepository.login).toBeDefined();
+    }));
+
+    it('should post login success',
+        inject(function (accountRepository, urlResolver, $httpBackend) {
+
+            $httpBackend.expect('POST', urlResolver.resolveAccount('Login'))
+                .respond(200, { success: true });
+
+            accountRepository.login({ loginId: 'test@test.com', password: 'password', rememberMe: true })
+                .success(function (data) {
+                    expect(data.success).toBeTruthy();
+                });
+
+            $httpBackend.flush();
+        }));
+
+    it("should have a logout method.", inject(function (accountRepository) {
+        expect(accountRepository.logout).toBeDefined();
+    }));
+
+    it('should get logout success',
+        inject(function (accountRepository, urlResolver, $httpBackend) {
+
+            $httpBackend.expect('GET', urlResolver.resolveAccount('Logout'))
+                .respond(200, { success: true });
+
+            accountRepository.logout()
+                .success(function (data) {
+                    expect(data.success).toBeTruthy();
+                });
+
+            $httpBackend.flush();
+        }));
+
+    it("should have a changePassword method.", inject(function (accountRepository) {
+        expect(accountRepository.changePassword).toBeDefined();
+    }));
+
+    it('should get password change success',
+        inject(function (accountRepository, urlResolver, $httpBackend) {
+
+            $httpBackend.expect('POST', urlResolver.resolveAccount('ChangePassword'))
+                .respond(200, { success: true });
+
+            accountRepository.changePassword({ oldPassword: "oldPassword", newPassword: "newPassword" })
+                .success(function (data) {
+                    expect(data.success).toBeTruthy();
+                });
+
+            $httpBackend.flush();
+        }));
+});
