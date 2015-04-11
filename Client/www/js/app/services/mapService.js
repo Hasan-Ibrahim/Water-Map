dd047f5bb0a17dec4d56e0e9bc31bd4ed92b2ab4
Nafeez Abrar <nafeez.abrar@jantrik.com>
2015-04-11 10:56:03 +0600

    Custom icon added for gps location

--- Client/www/js/app/services/mapService.js
+++ Client/www/js/app/services/mapService.js
@@ -29,8 +29,18 @@ lloydApp.factory('mapService', ['$http', '$q', 'serverUrl', 'jqHttp', 'geoLocati
 
     function trackLocation() {
         var map = getMap();
-
-        var marker = L.marker([29, 90]).addTo(map);
+        var icon = L.icon({
+            iconUrl: 'img/gpsIco.png',
+            shadowUrl: 'img/full-star.png',
+            iconSize: [38, 95], // size of the icon
+            shadowSize: [50, 64], // size of the shadow
+            iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+            shadowAnchor: [4, 62],  // the same for the shadow
+            popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
+        var marker = L.marker([29, 90], {
+            icon: icon
+        }).addTo(map);
         var circle = L.circle([29, 90], 5, {
             color: 'blue',
             fillColor: '#30B5F8',
@@ -119,7 +129,7 @@ lloydApp.factory('mapService', ['$http', '$q', 'serverUrl', 'jqHttp', 'geoLocati
                 SubscriptionTypes: subscriptions
             });
         },
-        postSourceAccessibility: function(sourceId, accessibility){
+        postSourceAccessibility: function (sourceId, accessibility) {
             return jqHttp.post(appRoot + "WaterSource/UpdateAccessibility", {
                 WaterSourceId: sourceId,
                 Accessibility: accessibility
