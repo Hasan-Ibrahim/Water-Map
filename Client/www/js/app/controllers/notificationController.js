eaddd270d7f4d5b78cf67d815e0a31eea5cc6406
hasan ibrahim <hasan.ibrahim@SHUVO-PC.jantrik.com>
2015-04-11 15:44:19 +0600

    issue fixed

--- Client/www/js/app/controllers/notificationController.js
+++ Client/www/js/app/controllers/notificationController.js
@@ -12,12 +12,12 @@ lloydApp.controller('notificationController', ['$scope', '$rootScope', 'sidebarS
 
         $scope.submit = function () {
             mapService.subscribeFeature(mapService.selectedSourceId, $scope.options);
-            $scope.options = null;
+            //$scope.options = null;
             $scope.closeNotificationWindow();
         };
 
         $scope.cancel  = function(){
-            $scope.options = null;
+            //$scope.options = null;
             $scope.closeNotificationWindow();
         };
 
