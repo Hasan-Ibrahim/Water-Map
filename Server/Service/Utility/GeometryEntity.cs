73e47b63364e637b2e9037ab1acfcf8e496a6924
Mohayemin <mohayeminul.islam@jantrik.com>
2015-04-11 06:23:50 +0600

    SourceType and MajorQuality sent with Geometry

--- Server/Service/Utility/GeometryEntity.cs
+++ Server/Service/Utility/GeometryEntity.cs
@@ -1,4 +1,5 @@
 ﻿using Data.Model;
+using Data.Model.Constants;
 
 namespace Service.Utility
 {
@@ -6,10 +7,35 @@ namespace Service.Utility
     {
         public int Id { get; set; }
         public string Geometry { get; set; }
-
+        public string SourceType { get; set; }
+        public Potability MajorQuality { get; set; }
+        
         public static GeometryEntity FromDbWaterSource(DbWaterSource dbWaterSource)
         {
-            return new GeometryEntity { Id = dbWaterSource.Id, Geometry = dbWaterSource.Shape.AsText() };
+            var majorQuality = Potability.Drinkable;
+            var majorQualityValue = dbWaterSource.PotableRatingCount;
+            if (majorQualityValue < dbWaterSource.ProcessableRatingCount)
+            {
+                majorQualityValue = dbWaterSource.ProcessableRatingCount;
+                majorQuality = Potability.NeedTreatment;
+            }
+            if (majorQualityValue < dbWaterSource.UnpotableRatingCount)
+            {
+                majorQualityValue = dbWaterSource.UnpotableRatingCount;
+                majorQuality = Potability.Undrinkable;
+            }
+            if (majorQualityValue < dbWaterSource.UnknownRatingCount)
+            {
+                majorQuality = Potability.Unknown;
+            }
+
+            return new GeometryEntity
+            {
+                Id = dbWaterSource.Id,
+                Geometry = dbWaterSource.Shape.AsText(),
+                SourceType = dbWaterSource.SourceType,
+                MajorQuality = majorQuality
+            };
         }
     }
 }
