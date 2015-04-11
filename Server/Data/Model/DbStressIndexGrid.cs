1a76a3ff668310a59a2d050c4a25239921748b73
Mohayemin <mohayeminul.islam@jantrik.com>
2015-04-11 13:33:19 +0600

    file added

---
+++ Server/Data/Model/DbStressIndexGrid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Data.Model.Base;
+
+namespace Data.Model
+{
+    [Table("StressIndexGrid")]
+    public class DbStressIndexGrid : DbModel
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public double StressIndex { get; set; }
+
+        [NotMapped]
+        public override DateTime LastUpdateTime { get; set; }
+        [NotMapped]
+        public override DateTime CreationTime { get; set; }
+    }
+}
